49248937ac82a93e50476ca6649d7e884ca46ee6
hiscor1 <hiscor1@DESKTOP-UP2FDOL>
2022-05-27 15:10:40 +0900

    브랜치 테스트중

--- gitTest/src/main/java/com/ikowon/gitTest/gitTest.java
+++ gitTest/src/main/java/com/ikowon/gitTest/gitTest.java
@@ -1,19 +1,17 @@
 package com.ikowon.gitTest;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class gitTest {
 
 	@RequestMapping(value = "/test")
 	public String home(Locale locale, Model model) {
+		System.out.println("테스트중");
 		return "test";
 	}
 	
