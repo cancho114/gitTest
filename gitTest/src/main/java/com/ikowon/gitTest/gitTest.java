e0c632e04ebea71d1446c7656a92a53f77a49295
hiscor1 <hiscor1@DESKTOP-UP2FDOL>
2022-05-27 16:42:56 +0900

    test

--- gitTest/src/main/java/com/ikowon/gitTest/gitTest.java
+++ gitTest/src/main/java/com/ikowon/gitTest/gitTest.java
@@ -12,8 +12,8 @@ public class gitTest {
 	@RequestMapping(value = "/test")
 	public String home(Locale locale, Model model) {
 		
-		System.out.println("1");
 		
+		System.out.println("1");
 		return "test";
 	}
 	
